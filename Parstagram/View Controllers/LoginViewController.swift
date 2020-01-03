b9b48543cba95104bc345c71e0141e9a576b7292
Jonathon Chenvert <jchenvert1@gmail.com>
2020-01-02 17:29:35 -0800

    Initial push to repo for Parstagram w/ completed required user stories

---
+++ Parstagram/View Controllers/LoginViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Jonathon Chenvert on 1/2/20.
+//  Copyright © 2020 Jonathon Chenvert. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil )
+            } else {
+                print("Login Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Signup Error: \(error?.localizedDescription)")
+            }
+        }
+
+    }
+    
+}
